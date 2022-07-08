5174014ab083481859410fd4e96ba142e82da0cf
Zeeshan Safdar <zeeshan7826@gmail.com>
2022-07-08 18:34:34 +0500

    add links for sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Link, Route, Switch } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar } from './components';
+import './App.css';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
 }
 
 export default App;
