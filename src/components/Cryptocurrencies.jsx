0a165d138a5fdc0da062648f01c69bffc3d3435c
Zeeshan Safdar <zeeshan7826@gmail.com>
2022-07-09 15:10:20 +0500

    make crypto currencies page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
 
-function Cryptocurrencies() {
-  return <div>Cryptocurrencies</div>;
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+function Cryptocurrencies({ simplified }) {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const filteredCryptos = cryptosList?.data?.coins.filter((crypto) =>
+      crypto.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setCryptos(filteredCryptos);
+  }, [cryptosList, searchTerm]);
+
+  if (isFetching) return 'Loading...';
+
+  return (
+    <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Crypto Currencies"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
+          <Col key={currency?.id} xs={24} sm={12} lg={6} className="crypto-card">
+            <Card
+              title={`${currency?.rank} ${currency?.name}`}
+              extra={<img className="crypto-image" src={currency?.iconUrl} alt="crypto" />}
+              hoverable
+            >
+              <p>Price: {millify(currency?.price)}</p>
+              <p>Market Cap: {millify(currency?.marketCap)}</p>
+              <p>Daily Change: {millify(currency?.change)}%</p>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
 
 export default Cryptocurrencies;
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,11 +3,12 @@ import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
 
 const { Title } = Typography;
 
 function Homepage() {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(100);
 
   if (isFetching) return 'Loading...';
 
@@ -35,6 +36,24 @@ function Homepage() {
           <Statistic title="Total Markets" value={millify(stats?.totalMarkets)} />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to={'/cryptocurrencies'}>Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to={'/news'}>Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 }
